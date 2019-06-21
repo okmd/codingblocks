aa5080fdfcedd0819ddf22319a96f323b55f9b9b
okmd <md.softdeveloper@gmail.com>
2019-06-21 12:56:27 +0530

    fundamental

--- challenge-fundamentals/help-ramu.cpp
+++ challenge-fundamentals/help-ramu.cpp
@@ -1,28 +1,35 @@
 #include<iostream>
 using namespace std;
 int main() {
-    
+    int test, c1,c2,c3,c4,n,m;
+    cin>>test;
+    while(test--){
+    	cin>>c1>>c2>>c3>>c4;//ticket price
+    	cin>>n>>m;//num of rickshaw, cabs ramu is going to use
+    	while(n--){ // ai, no. of times ramu will use rickshaw i 
+
+    	}
+    	while(m--){ // bi, no. of times ramu will use cab i
+
+    	}
+    }
     return 0;
 }
 
 /*
 
 Input: 
-    * 
-	1 
-	2 
-	/ 
-	4 
-	2 
-	+ 
-	3 
-	2 
-	; 
-	X
+2
+1 3 7 19
+2 3
+2 5
+4 4 4
+4 3 2 1
+1 3
+798
+1 2 3
 Output:
-    2 
-	2 
-	5 
-	Invalid operation. Try again.
+12
+1
 
 */
--- challenge-fundamentals/increasing-decreasing-sequence.cpp
+++ challenge-fundamentals/increasing-decreasing-sequence.cpp
@@ -1,19 +1,41 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main() {
-    int n,a,b; // both inc and dec
-    cin>>n;
-    cin>>a;
-    while(n-- -1){
 
+int main() {
+    int n,max=1000000000,a;
+    cin>>n;
+    while(n--)
+    {
+        cin>>a;
+        if(a>max){ // if we get large value than previous
+            max=a;
+            break;
+        }
+        max=a;            
+    }
+    while(n--)
+    {
+        cin>>a;
+        if(a<max){ // if decreasing 
+            cout<<"false\n";
+            return 0;
+        }
+        max=a;  
+    }
+    cout<<"true\n";
     return 0;
 }
 
 /*
 
 Input: 
-    5433231 
+    5
+
+    1
+    2
     3
+    4
+    0
 Output:
     3
 
