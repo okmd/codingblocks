85c7089b93868bd4db1f9c0cb7f27d06f4125f98
Danish <md.softdeveloper@gmail.com>
2019-08-01 10:48:01 +0530

    Floyd cycle

--- 20-linked-lists/12-floyd-cycle-detection.cpp
+++ 20-linked-lists/12-floyd-cycle-detection.cpp
@@ -2,15 +2,22 @@
 #include "01-linked-list.cpp"
 using namespace std;
 
+void chk(LL &a){
+	if(a.cycle())
+		cout<<"Yes!, Cycle is present\n";
+	else
+		cout<<"No!, Cycle is not present\n";
+}
 int main(){
 	LL a;
 	a.make_list();
 	a.print();
 	// merge
-	if(a.cycle())
-		cout<<"Yes!, Cycle is present\n";
-	else
-		cout<<"No!, Cycle is not present\n";
+	chk(a);
+	a.add_cycle(2,5);
+	chk(a);
+	a.remove_cycle();
+	chk(a);
 
 	return 0;
 }
