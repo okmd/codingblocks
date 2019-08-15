7dcc4c6ebcfd8fd8699a981a8c3de574f6add7de
okmd <md.softdeveloper@gmail.com>
2019-08-15 18:34:59 +0530

    histogram area

---
+++ 22-stack-and-queues/10-histogram-area-stack.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+
+int main(){
+
+	
+	return 0;
+}
+
+/*
+IMPORTANT: Interview Question.
+
+There are three methods to find solution
+
+1. Brute force: width*height between ith and jth bar.	O(n^2).
+2. Divide and Conquer. 									O(nlogn).
+3. Stack based solution.								O(n). 
+
+
+
+
+*/
