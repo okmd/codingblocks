d6c082ab4767370f2c28b130860a98e673eac3c7
okmd <md.softdeveloper@gmail.com>
2019-11-28 23:23:33 +0530

    graph introduction

---
+++ 33-graph-algorithms/01-introduction.cpp
@@ -0,0 +1,22 @@
+/*
+Finite set of nodes(vertices) and edges.
+Application: 
+    -> Google maps
+    -> Path finding algorithms
+    -> Social networking websites.(Facebook edge-rank algorithms)
+    -> Routing - server at different geolocation.
+
+Terminologies and components:
+    -> adjacent vertices ( O ---- O) adjacent node.
+    -> degree (of node) is # of edges connected to that node.
+        -> in-degree - incoming edges.
+        -> out-degree - outgoing edges.
+    -> path - way from one node to other.
+    -> subgraph - some path of graph.
+    -> connected graph - all nodes are connnected by atleast one edge (two connnected subgraph).
+    -> connected component - graph in which all nodes are connected is one component all such different graph act a component.
+    -> tree - graph without cycle (visit only one time).
+    -> forest - collection of trees (all connected component).
+    -> minimum spanning tree - weighted graph - all the nodes are covered with minimum weight - resulting tree is MST.
+    
+*/
