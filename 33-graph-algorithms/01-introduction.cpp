825fbd2ba82a45f99ff6a9bfc067b17e90ef70d9
okmd <md.softdeveloper@gmail.com>
2019-11-29 22:19:02 +0530

    adjacency list

--- 33-graph-algorithms/01-introduction.cpp
+++ 33-graph-algorithms/01-introduction.cpp
@@ -18,5 +18,24 @@ Terminologies and components:
     -> tree - graph without cycle (visit only one time).
     -> forest - collection of trees (all connected component).
     -> minimum spanning tree - weighted graph - all the nodes are covered with minimum weight - resulting tree is MST.
-    
+Types of graph:
+    -> directed edges - uni-directional
+    -> undirected edge - bi-directional
+    -> weighted edges - some value like distance
+    -> non weighted edges - no value, just an edge i.e connection.
+
+Number of edges in a graph
+    -> complete graph - O(n^2) edges:every node is connected with each other - nC2.
+    -> connected graph - range(n-1 to n^2): from tree to complete graph.
+    -> tree - O(n-1): every other node requires one node except root node.
+
+Implementation of graph:
+    1. List
+    -> V ={a,b,c,d} and E ={(a, b), (b, c), (d, a) .. }
+    -> both V and E are list.
+    2. Adjacency Matrix O(n^2) memory , not memory efficient, not used for finding neighbour.
+    -> VxV matrix, which store the edge between the edges.
+    -> wighted and non-weighted edges can be represented.
+    3. Adjacency list
+    -> node -> all nodes connected to this node is also a list.
 */
---
+++ 33-graph-algorithms/02-base-graph.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Grapgh{
+private:
+	int V;
+	list<int> *l;
+
+public:
+	Grapgh(int v){
+		V = v;
+		l = new list<int>[V]; // array of linked list
+	}
+	void addEdge(int u, int v, bool bidir=true){
+		l[u].push_back(v);
+		if(bidir)
+			l[v].push_back(u);
+	}
+	void print(){
+		for(int i=0; i<V; i++){
+			cout<<i<<"->";
+			for(int vertex:l[i]){
+				cout<<vertex<<",";
+			}
+			cout<<endl;
+		}
+	}
+
+};
+
+int main(){
+	Grapgh g(5); //0 to 4
+	g.addEdge(0,1);
+	g.addEdge(0,4);
+	g.addEdge(4,3);
+	g.addEdge(1,4);
+	g.addEdge(1,2);
+    g.addEdge(1,3);
+	g.addEdge(2,3);
+	g.print();
+	return 0;
+}
+
+/*
+Adjacency list representaion of graph.
+
+0 -> 1, 4
+1 -> 2 ,3 4, 0
+2 -> 1, 3
+3 -> 2, 1, 4
+4 -> 1, 0, 3
+
+*/
